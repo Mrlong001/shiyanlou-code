ea44c4d7f229ed8255bd822f2b80027f3f69a47c
Mrlong001 <1550237594@qq.com>
2019-09-27 00:33:38 +0800

    l commit

---
+++ jump7.py
@@ -0,0 +1,13 @@
+git clone https://github.com/Mrlong001/louplus.git
+ls
+cd loupuls
+wget https://labfile.oss.aliyuncs.com/courses/1330/linux.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python.1png.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/python.2png.png
+wget https://labfile.oss.aliyuncs.com/courses/1330/git.png
+git add --all
+git config --global user.name 'Mrlong001'
+git config --global user.email '1550237594'
+git commit -m 'l commit'
+git remote add l https://github.com/Mrlong001/louplus.git
+git push l master
